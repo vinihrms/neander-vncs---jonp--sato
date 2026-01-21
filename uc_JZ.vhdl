578968df53a494da8c768abadf59af001999fa94
vinihrms <viniciushermes13@gmail.com>
2026-01-21 11:18:50 -0300

    acho q matamo o leao

--- uc_JZ.vhdl
+++ uc_JZ.vhdl
@@ -9,17 +9,34 @@ ENTITY JZ IS
     );
 END ENTITY;
 
-ARCHITECTURE jz_arch OF JZ IS
-begin
-saida(10) <= NOT ciclo(2) and not flags(0);              -- snbarrINC
-saida(9)  <= NOT ciclo(2) AND ciclo(0);                   -- sPC_nrw
-saida(8)  <= (not ciclo(2) and ciclo(1) and not ciclo(0));   --ri                                       -- sRI_nrw
-saida(7)  <= '0';                                        -- sAC_nrw
-saida(6)  <= '0';                                        -- sMEM_nrw
-saida(5 downto 3) <= "000";                               -- sula_op
-saida(2) <= NOT ciclo(2) AND (ciclo(1) XNOR ciclo(0));    -- sREM_nrw
-saida(1) <= NOT ciclo(1) AND (ciclo(2) XOR ciclo(0));     -- sRDM_nrw
-saida(0) <= NOT ciclo(2) and flags(0);  -- snbarrPC
+ARCHITECTURE jayz OF JZ IS
+BEGIN
 
+    -- JZ - Jump Zero (se flags(0) = 1)
+
+    -- snbarrINC: quando Z=1, igual ao JMP; quando Z=0, sempre incrementa (nao pula)
+    saida(10) <= (NOT ciclo(2) OR (NOT ciclo(1) AND NOT ciclo(0))) WHEN flags(0) = '1' ELSE '1';
+
+    -- sPC_nrw: quando Z=1, carrega PC no ciclo 3; quando Z=0 nao carrega
+    saida(9) <= (NOT ciclo(1) AND ciclo(0)) WHEN flags(0) = '1' ELSE 
+                (NOT ciclo(2) AND ciclo(0));  -- incrementa normalmente se Z=0
+
+    -- sRI_nrw: igual ao JMP
+    saida(8) <= (NOT ciclo(2) AND ciclo(1) AND NOT ciclo(0));
+
+    saida(7) <= '0';  -- sAC_nrw
+    saida(6) <= '0';  -- sMEM_nrw
+    saida(5 DOWNTO 3) <= "000";  -- sula_op
+
+    -- sREM_nrw: igual ao JMP
+    saida(2) <= ((NOT ciclo(2) AND NOT ciclo(1) AND NOT ciclo(0)) OR 
+                 (NOT ciclo(2) AND ciclo(1) AND ciclo(0)));
+
+    -- sRDM_nrw: igual ao JMP
+    saida(1) <= ((NOT ciclo(2) AND NOT ciclo(1) AND ciclo(0)) OR 
+                 (ciclo(2) AND NOT ciclo(1) AND NOT ciclo(0)));
+
+    -- snbarrPC: quando Z=1, igual ao JMP endereco do barramento; quando Z=0 nao pula
+    saida(0) <= (NOT ciclo(2) OR NOT ciclo(1)) WHEN flags(0) = '1' ELSE '1';
 
 END ARCHITECTURE;
